f6036eaf54eedd4c37e2c875d9d938ad3a6d38dd
Vladyslav Aleksieiev <vladyslav.aleksieiev@nure.ua>
2020-07-16 21:13:39 +0300

    install react

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const App = () => (
+  <h1>Dota Fight Simulator</h1>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: './index.jsx',
   output: {
     filename: 'bundle.js',
   },
   resolve: {
+    extensions: ['*', '.js', '.jsx'],
     alias: {
       Root: path.resolve(__dirname, 'src'),
       Models: path.resolve(__dirname, 'src/models'),
@@ -19,6 +20,17 @@ module.exports = {
       template: 'index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   devServer: {
     open: true,
     port: 8080,
